067fef7a563357e41a61885ebdecc4f7ad2d23c4
Marcus <mlar5@kth.se>
2016-02-29 10:49:20 +0100

    paths modification and code cleaning

--- addpaths.m
+++ addpaths.m
@@ -1,5 +1,4 @@
-addpath('DeepLearToolbox/data');
+addpath('DeepLearnToolbox/data');
 addpath('DeepLearnToolbox/util');
 addpath('DeepLearnToolbox/NN');
 addpath('DeepLearnToolbox/DBN');
-addpath('DeepLearnToolbox/tests');
