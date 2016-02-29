edbe51257f91eaa2f7fd80e2f0d49de610b9b09c
Marcus <mlar5@kth.se>
2016-02-29 19:32:10 +0100

    NN test config

--- test_NN.m
+++ test_NN.m
@@ -7,8 +7,8 @@ train_y = double(train_y);
 test_y  = double(test_y);
 
 % normalize
-[train_x, mu, sigma] = zscore(train_x);
-test_x = normalize(test_x, mu, sigma);
+%[train_x, mu, sigma] = zscore(train_x);
+%test_x = normalize(test_x, mu, sigma);
 
 
 %% ex6 neural net with sigmoid activation and plotting of validation and training error
@@ -23,15 +23,21 @@ bestErr = 1;
 nns = [];
 opts_list = [];
 bestNN = 1;
-for nn_index = 1:5
-nn                      = nnsetup([784 500 150 10]);     
-nn.output               = 'softmax';                   %  use softmax output
-opts.numepochs          = 100*nn_index;                %  Number of full sweeps through data
+nn                      = nnsetup([784 500 150 10]);
+nn.learningRate = 0.1;
+%nn.momentum = 0;
+%nn.output               = 'softmax';                   %  use softmax output
+nn.activation_function = 'sigm';
+opts.numepochs          = 5;                %  Number of full sweeps through data
 opts.batchsize          = 100;                        %  Take a mean gradient step over this many samples
 opts.plot               = 0;                           %  enable plotting
-nn = nntrain(nn, tx, ty, opts, vx, vy);                %  nntrain takes validation set as last two arguments (optionally)
+nn.trained_epochs = 0;
+for nn_index = 1:100
 
+nn = nntrain(nn, train_x, train_y, opts);                %  nntrain takes validation set as last two arguments (optionally)
 [er, bad] = nntest(nn, test_x, test_y);
+nn.trained_epochs = nn.trained_epochs + opts.numepochs;
+
 if er<bestErr
 	bestErr = er;
 	bestNN = nn_index;
@@ -41,6 +47,6 @@ nns = [nns, nn];
 opts_list = [opts_list, opts];
 
 end
-save('NN_epochs_test.mat');
+save('NN_epochs_test_5.mat');
 
 %assert(er < 0.02, 'Too big error');
