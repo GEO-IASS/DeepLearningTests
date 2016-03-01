f822ef80f45906ab992a38a084fb0b0bc3689cac
Marcus Larson <Marcus Larson>
2016-03-01 09:40:32 +0100

    fixed variable name conflict

--- Results.m
+++ Results.m
@@ -1,11 +1,12 @@
 load('Results/NN_test_1.mat');
-load('Results/DBN_test_1.mat');
+nn.res = res;
+load('Results/DBN_test_2_bigBatch.mat');
 
 dbn_res = plot(res.dbns.trained_epochs, res.dbns.final_er);
 xlabel('trained epochs');
 ylabel('test error rate');
 hold on
-nn_res = plot(res.nns.trained_epochs, res.nns.final_er);
+nn_res = plot(nn.res.nns.trained_epochs, nn.res.nns.final_er);
 
 legend('DBN', 'NN');
 %legend(nn_res, 'NN');
